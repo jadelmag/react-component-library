3ebe05fda8e733eed396b3179253ecfe5791cd3c
Javi <kientienemibarraespaciadora@gmail.com>
2022-03-17 12:19:24 +0100

    updated rollup
    updated rollup

--- README.md
+++ README.md
@@ -3,9 +3,3 @@
 La librería de componentes para que pueda usarse en proyectos javascript y typescript.
 
 Además, incluye un tutorial para crear librerías de componentes en typescript, con pruebas y storybook desde cero.
-
-# TODO
-
-- Cambiar paths relativos por paths absolutos y que compile la librería.
-
-
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,7 @@ export interface ButtonProps {
   label: string;
 }
 
-const Button = ({ label }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ label }): JSX.Element => {
   return <button className="xu-button">{label}</button>
 }
 export default Button
-
-// const Button: FC<ButtonProps> = ({ label }): JSX.Element => {
-//   return <button>{label}</button>;
-// };
-// export default Button;
